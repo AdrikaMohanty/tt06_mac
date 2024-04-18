bec644111a7c7a1af9d366c12faa8bf6d798b7f8
AdrikaMohanty <84654826+AdrikaMohanty@users.noreply.github.com>
2024-04-19 01:35:36 +0530

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -88,47 +88,34 @@ endmodule
 module dlfloat_mac(clk,rst_n,a,b,c);
     input [15:0]a,b;
     input clk,rst_n;
-    output reg[15:0]c;
-    reg [15:0]c_out=0;
+    output bit[15:0]c;
+
     reg [15:0]data_a,data_b;
     wire [15:0]fprod,fadd;
     //dlfloat_mult(a,b,c,clk);
     //dlfloat_adder(input clk, input [15:0]a, input [15:0]b, output reg [15:0]c);
-	always @(posedge clk)
-	begin 
-	if(!rst_n)
-	begin 
-		data_a <= 16'b0;
-		data_b <= 16'b0;
-		
-		c <= 16'b0;
-	end 
-	else 
-	begin 
-		
-     
-        data_a <= a;
-        data_b <= b;
+    always @(posedge clk)
+    begin 
+	    if(!rst_n)
+	 	data_a <= 0;
+        	data_b <= 0;
+	    else 
+        	data_a <= a;
+        	data_b <= b;
         //fprod1 <= fprod;
         //c <= fadd;
     end 
-    end 
-    always @ (posedge clk)
-    begin 
-    c <= fadd;
-    c_out <= c;
-    end 
-//    always @ (posedge clk)
-//    begin 
-//    c_out <= c;
-//    end 
+	always @(posedge clk)
+		begin
+			c <= fadd;
+		end
+	
+    dlfloat_mult mul(data_a,data_b,fprod,clk);
+   dlfloat_adder add(clk,rst_n,fprod,c,fadd);
 
     
-    dlfloat_mult mul(data_a,data_b,fprod,clk);
-    dlfloat_adder add(clk,rst_n,fprod,c_out,fadd);
-    //dlfloat_adder add(clk,rst_n,fprod,c_out,fadd);
-    //assign c = rst_n ? fadd : c_out;
-endmodule
+    //assign c = fadd;
+endmodule 
 
 /////////////////
 module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output reg [15:0]c);
