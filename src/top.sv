ce0c25ed9f718858b0e5e658fc0045ce3bda8fff
AdrikaMohanty <84654826+AdrikaMohanty@users.noreply.github.com>
2024-04-18 19:39:27 +0530

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -1,5 +1,5 @@
 
-module top (
+module tt_um_mac (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
